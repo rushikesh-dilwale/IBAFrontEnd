571cf9d0e15dc078a3c10932fcf7f2f19fc73bf1
ShekharK23 <shekharkatkar71@gmail.com>
2023-11-20 20:33:05 +0530

    get all account

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GetallaccountsComponent } from './getallaccounts/getallaccounts.component';
+import { GetaccountbyIdComponent } from './getaccountby-id/getaccountby-id.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-  
-    GetallaccountsComponent
+    GetallaccountsComponent,
+    GetaccountbyIdComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class AdminModule { }
