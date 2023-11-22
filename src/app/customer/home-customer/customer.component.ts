2f444bf616c1993aaf320fe28be1e696bb7ff3a4
ShekharK23 <shekharkatkar71@gmail.com>
2023-11-22 14:30:49 +0530

    commit 22-11

--- src/app/customer/home-customer/customer.component.ts
+++ src/app/customer/home-customer/customer.component.ts
@@ -44,6 +44,7 @@ export class CustomerComponent implements OnInit{
         this.__adminService.dogetRegisteredAccount(userId).subscribe(
           (data) => {
             this.dto = data;
+            localStorage.setItem('accountId', this.dto.accountId.toString());
           },
           (err) => {
             console.log(err.err);
